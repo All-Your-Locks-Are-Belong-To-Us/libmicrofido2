8505b57431cda3a7e0fef1bda7bcda2a2cefff20
Felix Gohla <felix.gohla@student.hpi.de>
2022-06-22 09:42:54 +0200

    Add License Notices

--- examples/invalid_device.c
+++ examples/invalid_device.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022 Felix Gohla, Konrad Hanff, Tobias Kantusch,
+ *                    Quentin Kuth, Felix Roth. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #include "fido.h"
 
 int main(void) {
--- include/fido.h
+++ include/fido.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2018 Yubico AB. All rights reserved.
+ * Copyright (c) 2022 Felix Gohla, Konrad Hanff, Tobias Kantusch,
+ *                    Quentin Kuth, Felix Roth. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #pragma once
 
 #include <stddef.h>
