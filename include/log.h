2871265f75516dd5b161fe58d3fff401b965d89d
Felix Gohla <felix.gohla@student.hpi.de>
2022-06-22 09:56:14 +0200

    Add License Notices

--- include/log.h
+++ include/log.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2018-2021 Yubico AB. All rights reserved.
+ * Copyright (c) 2022 Felix Gohla, Konrad Hanff, Tobias Kantusch,
+ *                    Quentin Kuth, Felix Roth. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #pragma once
 
 // Since there is (currently) no way of logging, these functions do not do anything.
