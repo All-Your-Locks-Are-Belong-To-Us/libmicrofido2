89e16e154cc30b66d88999355381794e00783947
Tobias Kantusch <git@kantusch.dev>
2022-07-28 00:13:46 +0200

    Add measurement programs for Ed25519, Inflate, SHA256/512
    This adds the programs for measuring the time it takes to
    complete Ed25519, Inflate, and SHA256/512. Currently, these
    only really compile for the ATmega1284P, but the GPIO
    functionality can be adjusted for other boards as well.
    Moreover, this also adds some Python scripts to generate the
    input data for the measurement programs such as encryption
    keys.

---
+++ examples/measurements/scripts/gen_sha512.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+
+import hashlib
+
+hex_arrayify = lambda x: '{ ' + ', '.join([f'0x{i:02x}' for i in x]) + ' }'
+
+data = 576 * b'f'
+print(f'const uint8_t data[] = {hex_arrayify(data)};')
+
+hash = hashlib.sha512(data).digest()
+print(f'const uint8_t hash[] = {hex_arrayify(hash)};')
