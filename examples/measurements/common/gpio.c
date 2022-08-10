bd33012b9de428e8b0505bde1a949b3110925ece
Tobias Kantusch <git@kantusch.dev>
2022-08-10 18:48:04 +0200

    Make measurements compile for ESP32

--- examples/measurements/common/gpio.c
+++ examples/measurements/common/gpio.c
@@ -56,6 +56,57 @@ void delay(double ms) {
     k_sleep(K_MSEC(ms));
 }
 
+#elif defined(ESP_PLATFORM)
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "driver/gpio.h"
+
+#define PIN 7
+#define LED_PIN 19
+#define LED_PIN_MASK (1ULL << LED_PIN)
+#define PIN_MASK (1ULL << PIN)
+
+void setup_pin() {
+    // zero-initialize the config structure.
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        // set pin mask to only configure these specific pins
+        .pin_bit_mask = PIN_MASK,
+        .pull_down_en = 0,
+        .pull_up_en = 0
+    };
+    gpio_config(&io_conf);
+
+    // Disable on-board LED. Apparently, this seems to increase power consumption, so we leave it on.
+    // gpio_config_t led_io_conf = {
+    //     .intr_type = GPIO_INTR_DISABLE,
+    //     .mode = GPIO_MODE_OUTPUT,
+    //     // set pin mask to only configure these specific pins
+    //     .pin_bit_mask = LED_PIN_MASK,
+    //     .pull_down_en = 0,
+    //     .pull_up_en = 0
+    // };
+    // gpio_config(&led_io_conf);
+    // gpio_set_level(LED_PIN, 0);
+}
+
+void pin_on() {
+    gpio_set_level(PIN, 1);
+    // We could disable the FreeRTOS interrupts for every run, but that seems to have no effect on the overall performance.
+    // taskDISABLE_INTERRUPTS();
+}
+
+void pin_off() {
+    // taskENABLE_INTERRUPTS();
+    gpio_set_level(PIN, 0);
+}
+
+void delay(double ms) {
+    vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
 #else
 // To make the examples compile, default to NOOP.
 #warning GPIO does not work on this system, defaulting to nop
