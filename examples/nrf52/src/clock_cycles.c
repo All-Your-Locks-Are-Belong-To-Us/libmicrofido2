b873ac84e3ae1b91a52b28ecf6bb1f7526921a67
Felix Gohla <felix.gohla@student.hpi.de>
2022-08-10 10:34:43 +0200

    Allow Measuring Clock Cycles On nRF52

---
+++ examples/nrf52/src/clock_cycles.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 Felix Gohla, Konrad Hanff, Tobias Kantusch,
+ *                    Quentin Kuth, Felix Roth. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+#include <zephyr/timing/timing.h>
+
+// Buffer variable used for measuring elapsed clock cycles.
+timing_t clock_cycle_start;
--- examples/nrf52/src/main.c
+++ examples/nrf52/src/main.c
@@ -13,13 +13,15 @@
 
 #include <fido.h>
 
-#include "stateless_rp/stateless_rp.h"
-#include "stateless_rp/stateless_rp_nfc_simulator.h"
+#include "clock_cycles.h"
 #include "hw_crypto.h"
+#include "stateless_rp/stateless_rp_nfc_simulator.h"
+#include "stateless_rp/stateless_rp.h"
+
 
 int main(void) {
+    clock_init();
     if (init_hw_crypto() != 0) {
-        printk("Failed to initialize cryptography. Aborting.\n");
         return -1;
     } else {
         printk("Initialized cryptography.\n");
@@ -31,5 +33,9 @@ int main(void) {
         return -1;
     }
     const uint8_t updater_public_key[] = {0xA8, 0xEE, 0x4D, 0x2B, 0xD5, 0xAE, 0x09, 0x0A, 0xBC, 0xA9, 0x8A, 0x06, 0x6C, 0xA5, 0xB3, 0xA6, 0x22, 0x84, 0x89, 0xF5, 0x9E, 0x30, 0x90, 0x87, 0x65, 0x62, 0xB9, 0x79, 0x8A, 0xE7, 0x05, 0x15};
-    return stateless_assert(&dev, "example.com", updater_public_key);
+    clock_start_counting();
+    const int ret = stateless_assert(&dev, "example.com", updater_public_key);
+    uint64_t elapsed_cycles = clock_stop_counting();
+    printk("Elapsed cycles for stateless assertion: %zu\n", elapsed_cycles);
+    return ret;
 }
